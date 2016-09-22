53df48a7c89fad10ed3dc4b18906716e83da39a8
Kyle Bebak <kylebebak@gmail.com>
2016-09-21 21:10:24 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='my_sample',
+    version='',
+    description='Not the most useful package',
+    long_description='Check it out on GitHub...',
+    keywords='sample pip tutorial',
+    url='https://github.com/kylebebak/<repo>',
+    download_url = 'https://github.com/kylebebak/<repo>/tarball/<tag>',
+    author='kylebebak',
+    author_email='kylebebak@gmail.com',
+    license='MIT',
+    packages=['my_sample'],
+    install_requires=[],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+    ]
+)
---
+++ tests/test_sample.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class TestMySample(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
